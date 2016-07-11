682e2e101a0b22d9d7f5922aa89e06e06067efa8
DJ Roychoudhury and Ming Wen <ming@>
2016-07-11 09:21:14 -0700

    question is picked up by Log.

--- app/src/main/java/com/epicodus/discussionboard/MainActivity.java
+++ app/src/main/java/com/epicodus/discussionboard/MainActivity.java
@@ -4,28 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mNewQuestionButton;
+    private EditText mQuestionEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mQuestionEditText = (EditText) findViewById(R.id.questionEditText);
         mNewQuestionButton = (Button) findViewById(R.id.newQuestionButton);
-            mNewQuestionButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+        mNewQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String question = mQuestionEditText.getText().toString();
+                Log.d(TAG, question);
+                Intent intent = new Intent(MainActivity.this, SuccessActivity.class);
+                startActivity(intent);
 
-                    Intent intent = new Intent(MainActivity.this, SuccessActivity.class);
-                    startActivity(intent);
-
-                }
-            });
+            }
+        });
     }
 }
