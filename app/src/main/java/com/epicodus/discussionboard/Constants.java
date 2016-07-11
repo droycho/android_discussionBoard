ead66eeee64bb03f1a23f687497b85443f23dd87
DJ Roychoudhury and Ming Wen <ming@>
2016-07-11 10:34:52 -0700

    inputted data saves to firebase correctly.

---
+++ app/src/main/java/com/epicodus/discussionboard/Constants.java
@@ -0,0 +1,5 @@
+package com.epicodus.discussionboard;
+
+public class Constants {
+    public static final String FIREBASE_CHILD_QUESTION = "inputtedQuestion";
+}
--- app/src/main/java/com/epicodus/discussionboard/MainActivity.java
+++ app/src/main/java/com/epicodus/discussionboard/MainActivity.java
@@ -10,31 +10,43 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mNewQuestionButton;
-    private EditText mQuestionEditText;
+    @Bind(R.id.newQuestionButton) Button mNewQuestionButton;
+    @Bind(R.id.questionEditText) EditText mQuestionEditText;
+
+    private DatabaseReference mInputtedQuestionReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Bundle extras = getIntent().getExtras();
-        if(extras !=null) {
-            String value = extras.getString("questions");
-        }
-        setContentView(R.layout.activity_main);
 
-        mQuestionEditText = (EditText) findViewById(R.id.questionEditText);
-        mNewQuestionButton = (Button) findViewById(R.id.newQuestionButton);
+        mInputtedQuestionReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_QUESTION);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         mNewQuestionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String question = mQuestionEditText.getText().toString();
-                Log.d(TAG, question);
+                saveQuestionToFirebase(question);
+
                 Intent intent = new Intent(MainActivity.this, SuccessActivity.class);
                 intent.putExtra("question", question);
                 startActivity(intent);
-
+            }
+            public void saveQuestionToFirebase(String question) {
+                mInputtedQuestionReference.push().setValue(question);
             }
         });
     }
